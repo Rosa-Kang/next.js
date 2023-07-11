422145d3d57e7d7d2826a9d5ab1c8bbaa85bcd15
Rosa Kang <therosessom@gmail.com>
2023-07-11 14:44:57 -0700

    update input

--- app/components/modals/RegisterModal.tsx
+++ app/components/modals/RegisterModal.tsx
@@ -9,6 +9,7 @@ import { FieldValues, SubmitHandler, useForm } from 'react-hook-form';
 import useRegisterModal from '@/app/hooks/useRegisterModal';
 import Modal from './Modal';
 import Heading from '../Heading';
+import Input from '../inputs/input';
 
 const RegisterModal = () => {
     const registerModal = useRegisterModal();
@@ -36,6 +37,13 @@ const RegisterModal = () => {
            <Heading 
                 title="welcome to next-bnb!"
                 subtitle="Create an account!"
+            />
+            <Input
+     
+                label="Email"
+                disabled={isLoading}
+                register={register}
+                required
             />
         </div>
     )
