d40d6910b005894c39735a56368fefa0ede01b24
Rosa Kang <therosessom@gmail.com>
2023-07-11 13:54:19 -0700

    update start writing RegisterModal

--- app/hooks/useRegisterModal.tsx
+++ app/hooks/useRegisterModal.tsx
@@ -1,10 +1,16 @@
 'use client'
+import { create } from 'zustand';
 
-const useRegisterModal = () => {
-
-    return (
-        <></>
-    )
+interface RegisterModalStore {
+    isOpen: boolean;
+    onOpen: () => void;
+    onClose: () => void;
 }
+
+const useRegisterModal = create<RegisterModalStore>((set) => ({
+    isOpen: false,
+    onOpen: () => set({isOpen:true}),
+    onClose: () => set({isOpen: false})
+}))
 
 export default useRegisterModal;
