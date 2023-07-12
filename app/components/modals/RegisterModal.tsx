3e184317df4a4135899aa7b83bcf2942d761d748
Rosa Kang <therosessom@gmail.com>
2023-07-12 08:04:09 -0700

    import Toaster from react-hot-toast

--- app/components/modals/RegisterModal.tsx
+++ app/components/modals/RegisterModal.tsx
@@ -6,7 +6,7 @@ import {AiFillGithub} from 'react-icons/ai';
 import {FcGoogle} from 'react-icons/fc';
 import { useCallback, useState } from 'react';
 import { FieldValues, SubmitHandler, useForm } from 'react-hook-form';
-
+import useRegisterModal from '@/app/hooks/useRegisterModal';
 import Modal from './Modal';
 import Heading from '../Heading';
 import Input from '../inputs/input';
@@ -47,16 +47,17 @@ const RegisterModal = () => {
                 required
                 />
             <Input 
-                id="email"
-                label="Email"
+                id="name"
+                label="Name"
                 disabled={isLoading}
                 register={register}
                 errors={errors}
                 required
                 />
             <Input 
-                id='email'
-                label='Email'
+                id='password'
+                type='password'
+                label='Password'
                 disabled={isLoading}
                 register={register}
                 errors={errors}
