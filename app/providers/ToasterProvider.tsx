4a6d42906f72af7440167d37329dc3d4c3903921
Rosa Kang <therosessom@gmail.com>
2023-07-12 10:57:28 -0700

    connect database

--- app/providers/ToasterProvider.tsx
+++ app/providers/ToasterProvider.tsx
@@ -1,3 +1,7 @@
+/**
+ * Toaster is using useEffect so, we need at least one component that is 'use client' wrapping around the Toaster to use it. --> It is the reason we need ToasterProvider to wrap it.
+ */
+
 'use client';
 
 import { Toaster } from 'react-hot-toast';
