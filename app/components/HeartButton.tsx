46336563a071f3827692fa3e1555fdc524bbc410
Rosa Kang <therosessom@gmail.com>
2023-07-18 10:05:56 -0700

    add heartedButton

--- app/components/HeartButton.tsx
+++ app/components/HeartButton.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { AiOutlineHeart } from "react-icons/ai";
+import { AiFillHeart, AiOutlineHeart } from "react-icons/ai";
 import { SafeUser } from "../types";
 
 interface HeartButtonProps {
@@ -12,7 +12,7 @@ const HeartButton: React.FC<HeartButtonProps> = ({
     listingId, currentUser
 }) => {
 
-    const hadFavorited = false;
+    const hadFavorite = false;
     const toggleFavorite = () => { };
 
     return (
@@ -21,6 +21,8 @@ const HeartButton: React.FC<HeartButtonProps> = ({
             className="relative hover:opacity-80 transition cursor-pointer"
         >
             <AiOutlineHeart size={28} className="fill-white absolute -top-[2px] -right-[2px] " />
+
+            <AiFillHeart size={24} className={hadFavorite ? `fill-rose-500` : `fill-neutral-500/70` }   />
         </div>
 
         )
