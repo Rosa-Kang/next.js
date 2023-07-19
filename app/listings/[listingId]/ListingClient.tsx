a886ff06c0904b50e3f247ee33b2de2c6cd458a4
Rosa Kang <therosessom@gmail.com>
2023-07-18 18:14:39 -0700

    add listingHead

--- app/listings/[listingId]/ListingClient.tsx
+++ app/listings/[listingId]/ListingClient.tsx
@@ -1,8 +1,12 @@
+'use client';
+
 import { Reservation } from ".prisma/client";
-import {categories} from "@/app/components/navbar/Categories";
+import { categories } from "@/app/components/navbar/Categories";
 
 import { SafeUser, safeListing } from "@/app/types";
+import Container from "@/app/components/Container";
 import { useMemo } from "react";
+import ListingHead from "@/app/components/listings/ListingHead";
 
 interface ListingClientProps {
     reservations?: Reservation[];
@@ -22,7 +26,21 @@ const ListingClient: React.FC<ListingClientProps> = ({
         })
     }, [listing.category]);
 
-    return (<></>)
+    return (
+        <Container>
+            <div className="max-w-screen-lg mx-auto">
+                <div className="flex flex-col gap-6">
+                    <ListingHead
+                        title={listing.title}
+                        imageSrc={listing.imageSrc}
+                        locationValue={listing.locationValue}
+                        id={listing.id}
+                        currentUser={currentUser}
+                    />
+                </div>
+            </div>
+        </Container>
+    )
 }
 
 export default ListingClient; 
