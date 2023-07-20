e7a078e150037620a7fb7ae16edc0d7afdfe4907
Rosa Kang <therosessom@gmail.com>
2023-07-19 19:02:44 -0700

    add trip path to the server

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -9,12 +9,14 @@ import useRegisterModal from '@/app/hooks/useRegisterModal';
 import useLoginModal from '@/app/hooks/useLoginModal';
 import useRentModal from '@/app/hooks/useRentModal';
 import { SafeUser } from '@/app/types';
+import {useRouter } from 'next/navigation';
 
 interface UserMenuProps {
     currentUser?: SafeUser | null
 }
 
-const UserMenu: React.FC<UserMenuProps> = ({currentUser}) => {
+const UserMenu: React.FC<UserMenuProps> = ({ currentUser }) => {
+    const router = useRouter();
     const registerModal = useRegisterModal();
     const loginModal = useLoginModal();
     const [isOpen, setIsOpen] = useState(false);
@@ -56,7 +58,7 @@ const UserMenu: React.FC<UserMenuProps> = ({currentUser}) => {
                         {currentUser ? (
                             <>
                                 <MenuItem 
-                                onClick={()=> {}} 
+                                onClick={()=> router.push('/trips')} 
                                 label="My trips"/>
                                 <MenuItem 
                                 onClick={()=> {}}
