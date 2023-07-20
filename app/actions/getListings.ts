6c5f30ebf4103090d2f8aa07d3bcd099ce6019e1
Rosa Kang <therosessom@gmail.com>
2023-07-20 14:08:32 -0700

    add filter into getListings action

--- app/actions/getListings.ts
+++ app/actions/getListings.ts
@@ -2,18 +2,81 @@ import prisma from '@/app/libs/prismadb';
 
 export interface IListingsParams {
     userId?: string;
+    guestCount?: number;
+    roomCount?: number;
+    bathroomCount?: number;
+    startDate?: string;
+    endDate?: string;
+    locationValue?: string;
+    category?: string;
 }
 
 export default async function getListings(
     params: IListingsParams
 ) {
     try {
-        const { userId } = params;
+        const {
+            userId,
+            guestCount,
+            roomCount,
+            bathroomCount,
+            startDate,
+            endDate,
+            locationValue,
+            category
+        } = params;
 
         let query: any = {};
 
         if (userId) {
             query.userId = userId;
+        }
+
+        if (category) {
+            query.category = category;
+        }
+
+        if (locationValue) {
+            query.locationValue = locationValue;
+        }
+
+        if (roomCount) {
+            query.roomCount = {
+                //greater than equal(gte) : sort anything greater or equal to the number of chosen
+                gte: +roomCount
+            }
+        }
+
+        if (guestCount) {
+            query.guestCount = {
+                gte : +guestCount
+            }
+        }
+
+        if (bathroomCount) {
+            query.bathroomCount = {
+                gte : +bathroomCount
+            }
+        }
+        
+        //filter-out the Date
+        if (startDate && endDate) {
+            query.NOT = {
+                reservations: {
+                    some: {
+                        OR: [
+                            {
+                                endDate: {gte : startDate},
+                                startDate: {lte : startDate},
+                            },
+                            { 
+                                startDate: { lte: endDate },
+                                endDate: { gte: endDate },
+                            }
+                        ]
+                    }
+                }
+            }
         }
 
 
