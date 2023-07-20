e6871a4c77dcea8a051565e5b8c47aa1cf36516b
Rosa Kang <therosessom@gmail.com>
2023-07-20 11:01:46 -0700

    add useRouter to SearchModal

---
+++ app/components/modals/SearchModal.tsx
@@ -0,0 +1,23 @@
+'use client'
+
+import { useRouter } from "next/navigation";
+import Modal from "./Modal";
+
+import useSearchModal from "@/app/hooks/useSearchModal";
+
+const SearchModal = () => {
+    const router = useRouter();
+    const searchModal = useSearchModal();
+
+    return (
+        <Modal
+            isOpen={searchModal.isOpen}
+            onClose={searchModal.onClose}
+            onSubmit={searchModal.onOpen}
+            title="Filters"
+            actionLabel="Search"
+        />
+        )
+}
+
+export default SearchModal;
--- app/components/navbar/Search.tsx
+++ app/components/navbar/Search.tsx
@@ -1,21 +1,26 @@
 'use client';
+import useSearchModal from '@/app/hooks/useSearchModal';
 import {BiSearch} from 'react-icons/bi'
 
-const Search =() => {
+const Search = () => {
+    const searchModal = useSearchModal();
+
     return (
-<div className="border-[1] w-full md:w-auto py-2 flex flex-row items-center rounded-full shadow-sm hover:shadow-md transition cursor-pointer" >
-    <div className="flex flex-row items-center justify-between">
-        <div className="text-sm font-semibold px-6">Any where</div>
-        <div className="hidden sm:block text-sm font-semibold px-6 border-x-[1px] flex-w text-center">Any week</div>
-    </div>
-    
-    <div className="text-sm pl-6 pr-2 text-gray-600 flex flex-row items-center gap-3">
-        <div className='hidden sm:block'>Add Guest</div>
-        <div className="p-2 bg-rose-500 rounded-full text-white">
-        <BiSearch size={18} />
+        <div
+            onClick={searchModal.onOpen}
+            className="border-[1] w-full md:w-auto py-2 flex flex-row items-center rounded-full shadow-sm hover:shadow-md transition cursor-pointer" >
+                <div className="flex flex-row items-center justify-between">
+                    <div className="text-sm font-semibold px-6">Any where</div>
+                    <div className="hidden sm:block text-sm font-semibold px-6 border-x-[1px] flex-w text-center">Any week</div>
+                </div>
+                
+                <div className="text-sm pl-6 pr-2 text-gray-600 flex flex-row items-center gap-3">
+                    <div className='hidden sm:block'>Add Guest</div>
+                    <div className="p-2 bg-rose-500 rounded-full text-white">
+                    <BiSearch size={18} />
+                    </div>
+                </div>
         </div>
-    </div>
-</div>
     )
 }
 
