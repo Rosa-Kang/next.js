f6180b4e9719364d0339fb897b754d50543a4440
Rosa Kang <therosessom@gmail.com>
2023-07-22 08:55:53 -0700

    add prep for deploy

---
+++ middleware.ts
@@ -0,0 +1,12 @@
+
+
+export {default} from 'next-auth/middleware';
+
+export const config = {
+    marcher: [
+        '/trips',
+        '/reservations',
+        '/properties',
+        '/favorites',
+    ]
+}
